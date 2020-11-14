319d58bc34723d3493e2ff0468a67f6319ba6c61
BismarckOliveira <bismarckoliver@hotmail.com>
2020-11-14 00:19:57 -0300

    Configurando Rotas

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
-import express from 'express';
+import express, { json } from 'express';
 import routes from './routes';
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 app.get('/', (request, response) =>
   response.json({ message: 'Bismarck Oliveira' }),
