34f796279a38dc69362a6e293b26f47b9635b129
BismarckOliveira <bismarckoliver@hotmail.com>
2020-11-15 17:50:57 -0300

    Trabalhando com principios do SOLID

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,37 @@
+import { startOfHour } from 'date-fns';
+import Appointment from '../models/appointment';
+import AppointmentsRepository from '../repositories/appointmentsRepository';
+
+interface request {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ date, provider }: request): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointmente is already booked ');
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
