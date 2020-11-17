5af6701e2c389d3406297ae631de5483fbec2f04
BismarckOliveira <bismarckoliver@hotmail.com>
2020-11-17 19:59:35 -0300

    Configurando Migration

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import routes from './routes';
 
+import './database';
+
 const app = express();
 
 app.use(express.json());
@@ -11,5 +13,5 @@ app.get('/', (request, response) =>
 );
 
 app.listen(3333, () => {
-  console.log('Server started on port 3333');
+  console.log('Server started on port 3333!');
 });
