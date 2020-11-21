bea826fc54225ef970981e705d0e699bd74745cf
BismarckOliveira <bismarckoliver@hotmail.com>
2020-11-20 22:56:20 -0300

    Criptografia senha HASH

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
-import CreateAppointmenteService from '../services/CreateAppointmentService';
+import CreateAppointmenteService from '../services/CreateAppointmenteService';
 import AppointmentRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
@@ -15,7 +15,7 @@ appointmentsRouter.get('/', async (request, response) => {
 
 appointmentsRouter.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { provider_id, date } = request.body;
 
     const parsedDate = parseISO(date);
 
@@ -23,7 +23,7 @@ appointmentsRouter.post('/', async (request, response) => {
 
     const appointment = await createAppointment.execute({
       date: parsedDate,
-      provider,
+      provider_id,
     });
 
     return response.json(appointment);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,12 @@
 import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
 
+import CreateUserService from '../services/CreateUserService';
+import usersRouter from './users.routes';
+
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
