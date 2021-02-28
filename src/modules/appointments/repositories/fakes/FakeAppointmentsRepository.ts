151c768e14c239ec83415682c15208f4baa826f2
BismarckOliveira <bismarckoliver@hotmail.com>
2021-02-28 19:03:29 -0300

    jest

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,17 +1,32 @@
-
+import { uuid } from 'uuidv4'
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository'
 
 import ICreateAppointimentDTO from '@modules/appointments/dtos/ICreateAppointimentDTO'
 import Appointment from '../../infra/typeorm/entities/Appointment';
 
 class AppointmentsRepository implements IAppointmentsRepository {
+  private appointments: Appointment[] = [];
 
-  public async findByDate(date: Date): Promise<Appointment | undefined> {}
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(appointments =>
+      appointments.date  === date
+      );
+
+      return findAppointment;
+  }
 
   public async create({
     date,
     provider_id
-  }: ICreateAppointimentDTO): Promise<Appointment> {}
+  }: ICreateAppointimentDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    Object.assign(appointment, { id: uuid(), date, provider_id})
+
+    this.appointments.push(appointment)
+
+    return appointment;
+  }
 }
 
 export default AppointmentsRepository;
